8836682a9701a16fbe152f82b737cbe10cdaf658
wiki <charmfocus@gmail.com>
2017-08-23 20:07:21 +0800

    用户ua信息功能增加

--- hangout-filters/hangout-filters-ua/src/main/java/com/ctrip/ops/sysdev/filters/UA.java
+++ hangout-filters/hangout-filters-ua/src/main/java/com/ctrip/ops/sysdev/filters/UA.java
@@ -1,35 +1,42 @@
 package com.ctrip.ops.sysdev.filters;
 
 import com.ctrip.ops.sysdev.baseplugin.BaseFilter;
-import lombok.extern.log4j.Log4j2;
+import org.apache.log4j.Logger;
 import ua_parser.Client;
 import ua_parser.Parser;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
-@Log4j2
 public class UA extends BaseFilter {
-
+	private static final Logger logger = Logger.getLogger(UA.class.getName());
 
 	public UA(Map config) {
 		super(config);
 	}
 
 	private String source;
+	private String target;
 	private Parser uaParser;
 
 	protected void prepare() {
 		if (!config.containsKey("source")) {
-			log.error("no field configured in Json");
+			logger.error("no field configured in Json");
 			System.exit(1);
 		}
 		this.source = (String) config.get("source");
 
+		if (config.containsKey("target")) {
+			this.target = (String) config.get("target");
+		} else {
+			this.target = "ua";
+		}
+
 		try {
 			this.uaParser = new Parser();
 		} catch (IOException e) {
-			log.error(e);
+			logger.error(e);
 			e.printStackTrace();
 			System.exit(1);
 		}
@@ -38,17 +45,24 @@ public class UA extends BaseFilter {
 	@SuppressWarnings("unchecked")
 	@Override
 	protected Map filter(final Map event) {
+		HashMap targetObj = new HashMap();
 		if (event.containsKey(this.source)) {
 			Client c = uaParser.parse((String) event.get(this.source));
-
-			event.put("userAgent_family", c.userAgent.family);
-			event.put("userAgent_major", c.userAgent.major);
-			event.put("userAgent_minor", c.userAgent.minor);
-			event.put("os_family", c.os.family);
-			event.put("os_major", c.os.major);
-			event.put("os_minor", c.os.minor);
-			event.put("device_family", c.device.family);
+			targetObj.put("userAgent_family", c.userAgent.family);
+			targetObj.put("userAgent_major", c.userAgent.major);
+			targetObj.put("userAgent_minor", c.userAgent.minor);
+			targetObj.put("os_family", c.os.family);
+			targetObj.put("os_major", c.os.major);
+			targetObj.put("os_minor", c.os.minor);
+			targetObj.put("device_family", c.device.family);
 		}
+
+		if (this.target != null) {
+			event.put(this.target, targetObj);
+		} else {
+			event.putAll(targetObj);
+		}
+
 		return event;
 	}
 }
